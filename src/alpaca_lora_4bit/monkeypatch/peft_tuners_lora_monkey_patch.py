39a6fa1a881f99374573036ef3847875a1024542
Wing Lian <wing.lian@gmail.com>
2023-04-16 18:24:57 -0400

    add missing import

--- src/alpaca_lora_4bit/monkeypatch/peft_tuners_lora_monkey_patch.py
+++ src/alpaca_lora_4bit/monkeypatch/peft_tuners_lora_monkey_patch.py
@@ -1,4 +1,5 @@
 def replace_peft_model_with_int4_lora_model():
     import peft.peft_model
+    from peft import PeftType
     from alpaca_lora_4bit.models import GPTQLoraModel
     peft.peft_model.PEFT_TYPE_TO_MODEL_MAPPING[PeftType.LORA] = GPTQLoraModel
