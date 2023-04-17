d553bf39730e078aa23ef696a9d9a9c15a8a6790
Wing Lian <wing.lian@gmail.com>
2023-04-17 11:20:57 -0400

    move the cuda kernel into this repo from https://github.com/qwopqwop200/GPTQ-for-LLaMa/tree/cuda

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from setuptools import setup, find_packages
+import sys
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 install_requires = []
 with open("./requirements.txt", "r") as requirements_file:
@@ -6,6 +8,21 @@ with open("./requirements.txt", "r") as requirements_file:
     reqs = [r.strip() for r in requirements_file.readlines() if "gptq_llama" not in r]
     for r in reqs:
         install_requires.append(r)
+
+quant_cuda_module = CUDAExtension(
+    'alpaca_lora_4bit.quant_cuda',
+    sources=[
+        'src/alpaca_lora_4bit/quant_cuda/quant_cuda.cpp',
+        'src/alpaca_lora_4bit/quant_cuda/quant_cuda_kernel.cu'
+    ])
+
+# conditionally only install the cuda extension explicitly
+ext_modules = []
+cmdclass = {}
+if '--cuda' in sys.argv or any(["cuda" in arg for arg in sys.argv]):
+    ext_modules.append(quant_cuda_module)
+    cmdclass = {'build_ext': BuildExtension}
+    sys.argv.remove('--cuda')
 
 
 setup(
@@ -19,4 +36,6 @@ setup(
         'cuda': 'gptq_llama @ git+https://github.com/sterlind/GPTQ-for-LLaMa.git@lora_4bit',
         'triton': 'triton',
     },
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
 )
