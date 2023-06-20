05f55c010a571dfb15fa9799e444d3c203429045
Alexander Pozharskii <gaussmake@gmail.com>
2023-06-21 03:13:56 +0400

    Remove import of custom_autotune (it's triton-dependent, triton dependency is optional)

--- src/alpaca_lora_4bit/__init__.py
+++ src/alpaca_lora_4bit/__init__.py
@@ -2,7 +2,6 @@ from . import monkeypatch
 from . import amp_wrapper
 from . import arg_parser
 from . import autograd_4bit
-from . import custom_autotune
 from . import Finetune4bConfig
 from . import gradient_checkpointing
 from . import models
@@ -10,3 +9,4 @@ from . import train_data
 # We don't import these automatically as it is dependent on whether we need cuda or triton
 # from . import matmul_utils_4bit
 # from . import triton_utils
+# from . import custom_autotune
